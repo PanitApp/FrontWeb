6cae171e695b376dbed17f4340a858a5fbbc8be4
daniel19066 <dpallaresr@unal.edu.co>
2020-10-13 23:59:09 -0500

    form de inicio

--- src/router.js
+++ src/router.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'Home',
+      component: () => import("@/views/Home.vue"),
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: () => import("@/views/Register.vue"),
     }
   ]
 })
