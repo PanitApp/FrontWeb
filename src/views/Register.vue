a070aa40f7fc31459e34befce8980ae4b3b84947
daniel19066 <dpallaresr@unal.edu.co>
2020-10-19 10:51:33 -0500

    ...

---
+++ src/views/Register.vue
@@ -0,0 +1,35 @@
+
+<template>
+
+  <!-- Material form login -->
+    <div class="container"> 
+
+        
+        <form>
+        <p class="h4 text-center mb-4 ">Iniciar sesion</p>
+        <div class="grey-text">
+        <mdb-input label="correo"  type="email"/>
+        <mdb-input label="contraseña"  type="password"/>
+        </div>
+        <div class="text-center">
+        <button class="btn" style="background-color:#05DFEB;" type="submit">Iniciar</button>
+        </div>
+    </form>
+    </div>
+
+  <!-- Material form login -->
+</template>
+
+<script>
+  import { mdbInput } from 'mdbvue';
+  
+  export default {
+    name: 'Register',
+    components: {
+      mdbInput
+    },
+  }
+</script>
+
+<style scoped>
+</style>
