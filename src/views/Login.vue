c02920ba56a01c2a6ee24f8b1d92b3dcbbf7625e
gapuentesr <gapuentesr@unal.edu.co>
2020-10-22 00:11:25 -0500

    Nuevo diseño Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,63 +1,78 @@
 <template>
-  <!-- Material form login -->
-    <div class="container"> 
+  <mdb-row id="view">
+    <mdb-col sm="4">
+    </mdb-col>  
+        <mdb-col sm="4" style="margin-top: 100px;">
+          <mdb-card narrow>
+            <mdb-view hover cascade>
+              <a href="#!">
+                <mdb-card-image :src="require('@/assets/Inicio.png')" alt="Card image cap" style="margin-left: 30px; margin-right: 30px;"></mdb-card-image>
+                <mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
+              </a>
+            </mdb-view>
+            <mdb-card-body cascade>
 
-        
-        <form>
-        <p class="h4 text-center mb-4 ">Iniciar sesion</p>
-        <div class="grey-text">
-        <mdb-input v-model="newUsername" label="nombre de usuario"  type="text"/>
-        <mdb-input v-model="newContrasena" label="contraseña"  type="password"/>
-        <p>{{newUsername}}</p>
-        <p>{{newContrasena}}</p>
-        </div>
-        <div class="text-center">
-          <ApolloQuery
-      :query="gql => gql`
-        query  ($newUsername: String!) {
-          getUsuarioByUsername(nombre_usuario: $newUsername){
-           ...usuario
-          }
-        }
-        ${$options.fragments.usuario}
-      `"
-      :variables="{ newUsername }"
-    >
-      <template slot-scope="{ result: { loading, error, data } }">
-        <!-- Loading -->
-        <div v-if="loading" class="loading apollo">Loading...</div>
+              <form class="text-center border border-light p-5">
+                <p class="h4 text-center mb-4 ">Iniciar sesion</p>
+                <mdb-input v-model="newUsername" label="Nombre de usuario"  type="text"/>
+                <mdb-input v-model="newContrasena" label="Contraseña"  type="password"/>
+                <p>{{newUsername}}</p>
+                <p>{{newContrasena}}</p>
+                
+            
+                <div class="text-center">
+                  <ApolloQuery
+                    :query="gql => gql`
+                    query  ($newUsername: String!) {
+                    getUsuarioByUsername(nombre_usuario: $newUsername){
+                    ...usuario
+                    }
+                  }
+                  ${$options.fragments.usuario}
+                  `"
+                  :variables="{ newUsername }"
+                >
+                    <template slot-scope="{ result: { loading, error, data } }">
+                      <!-- Loading -->
+                      <div v-if="loading" class="loading apollo">Loading...</div>
 
-        <!-- Error -->
-        <div v-else-if="error" class="error apollo">An error occured: {{error}}</div>
+                      <!-- Error -->
+                      <div v-else-if="error" class="error apollo">An error occured: {{error}}</div>
 
-        <!-- Result -->
-        <div v-else-if="data.getUsuarioByUsername[0]!=null" class="result apollo">{{ data.getUsuarioByUsername[0].contrasena }}
-          <p v-if="data.getUsuarioByUsername[0].contrasena==newContrasena">contraseña correcta</p>
-        </div>
+                      <!-- Result -->
+                      <div v-else-if="data.getUsuarioByUsername[0]!=null" class="result apollo">{{ data.getUsuarioByUsername[0].contrasena }}
+                        <p v-if="data.getUsuarioByUsername[0].contrasena==newContrasena">contraseña correcta</p>
+                      </div>
 
-        <!-- No result -->
-        <div v-else class="no-result apollo">No result :(</div>
-        <button class="btn" v-on:click="login(data)" style="background-color:#05DFEB;" type="button">Iniciar</button>
-        <p>{{id}}</p>
-        <p>{{autenticado}}</p>
-        <p>{{idstore}}</p>
-      </template>
-    </ApolloQuery>
-
-        </div>
-    </form>
-    </div>
-
-  <!-- Material form login -->
+                      <!-- No result -->
+                      <div v-else class="no-result apollo">No result :(</div>
+                      <button class="btn" v-on:click="login(data)" style="background-color:#05DFEB;" type="button">Iniciar</button>
+                      <p>{{id}}</p>
+                      <p>{{autenticado}}</p>
+                      <p>{{idstore}}</p>
+                    </template>
+                  </ApolloQuery>
+                </div>
+              </form>
+            </mdb-card-body>
+          </mdb-card>
+        </mdb-col>
+    </mdb-row>  
 </template>
 
 <script>
-  import { mdbInput } from 'mdbvue';
+  import {  mdbRow, mdbCol, mdbCard, mdbCardImage, mdbCardBody, mdbInput } from 'mdbvue';
   import gql from 'graphql-tag'
   export default {
     name: 'Register',
     components: {
-      mdbInput
+      mdbInput,
+
+			mdbRow,
+			mdbCol,
+			mdbCard,
+			mdbCardImage,
+			mdbCardBody
     },
     fragments: {
     usuario: gql`
@@ -119,4 +134,8 @@
 </script>
 
 <style scoped>
+  #view{
+    background-image: url('https://mdbootstrap.com/img/Photos/Others/architecture.jpg');
+    height: 100vh;
+  }
 </style>
